24dbb33e1e364a1373c5b04d114f7b7677ef9262
MissCatLady <citrus.smooth@gmail.com>
2014-09-22 13:24:05 -0400

    added friend list, new friend, at switches

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from flask import Flask, make_response, render_template, redirect, request, flash, session, url_for, escape
 from sqlalchemy import *
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SelectField
 from wtforms.validators import DataRequired, Email
 from flask.ext.scrypt import generate_password_hash, generate_random_salt, check_password_hash
-import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 engine = create_engine('postgresql://localhost:5432/alarmdb', pool_size=20, max_overflow=0)
@@ -15,6 +15,7 @@ app.config.update(
 )
 
 users = Table('users', metadata, autoload=True)
+friend = Table('friends', metadata, autoload=True)
 
 
 def validate_login():
@@ -34,16 +35,23 @@ class LogIn(Form):
 	email = StringField('email', validators=[DataRequired()])
 	password = PasswordField('password', validators=[DataRequired()])
 
+class NewFriend(Form):
+	email = StringField("E-mail", validators=[DataRequired()])
 
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
 	signup = SignUp(prefix="signup")
 	login = LogIn(prefix="login")
+	newfriend = NewFriend(prefix="friend")
+	friends = getfriends()
+		
 	if request.method =='GET':
 		user = validate_login();
 		if(user):
-			return render_template('dashboard.html', username=user.username)
+
+			
+			return render_template('dashboard.html', username=user.username, user=user, friends=friends, newfriend=newfriend, permissions=getpermissions())
 		
 		return render_template('index.html', signup=signup, login=login)
 	else:
@@ -55,45 +63,22 @@ def index():
 			print "new user"
 			#TODO validate unique email
 			return new_user(signup.email.data, signup.username.data, signup.password.data)
+
+		elif newfriend.validate_on_submit() and request.form['btn'] == 'Add Friend':
+			return addfriend(newfriend)
 		else:
 			flash('Error')
 			return render_template('index.html', signup=signup)
 
 
-
-@app.route('/main')
-def dashboard(email, password):
-
- 	try:
-		user = users.select(users.c.email == email).execute().first()
-		username = user.username
-
-		if check_password_hash(password, user.password, user.salt):
-			session_token = generate_password_hash(email, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-			stmt = update(users).where(users.c.email==email).values(session_token=session_token)
-			connection = engine.connect()
-			connection.execute(stmt)
-			connection.close()
-
-			resp = make_response(render_template('dashboard.html', username=username))
-			resp.set_cookie('session_token', session_token);
-			return resp;
-		else:
-			error_msg = "Incorrect Password"
-			return render_template('/index.html', error_msg=error_msg)
-
-	except AttributeError:
-		error_msg = "no user exists"
-		return render_template('index.html', error_msg=error_msg)
-	
-
 @app.route('/registration')
 def new_user(email, username, password):
 
-	#selt + hashing
+	#salt + hashing
 	salt = generate_random_salt()
 	password = generate_password_hash(password, salt)
 	session_token = generate_password_hash(email, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+	
 	#insert user information
 	user_entry = users.insert().values(username=username, email=email, 
 		password=password, salt=salt, session_token = session_token)
@@ -102,9 +87,53 @@ def new_user(email, username, password):
 	print res.inserted_primary_key
 	connection.close()
 	
-	resp = make_response(render_template('dashboard.html', new_user="Thanks for registering!", username=username))
+	resp = make_response(render_template('dashboard.html', alert="Thanks for registering!", username=username))
 	resp.set_cookie('session_token', session_token);
 	return resp;
+	
+@app.route('/setalarm', methods=('GET', 'POST')) 
+def setalarm():
+
+	user = validate_login()
+
+	if (user): 
+
+		hh = request.args.get('hours')
+		mm = request.args.get('mins')
+		mers = request.args.get('meridian')
+
+		#Reformat date into unambigious string.
+		#From 01/01/2000 to Jan 01 2000
+		date = request.args.get('date')
+		date_obj = datetime.strptime(date, "%m/%d/%Y")
+		date = date_obj.strftime('%b %d %Y')
+		
+		uid = int(request.args.get('uid'))
+		#friend = request.args.get('friend')
+
+		time = hh + ":" + mm + " " + mers
+
+		#Insert alarm.
+		alarms = Table('alarms', metadata, autoload=True)
+		alarm_entry = alarms.insert().values(uid=uid, date=date, time=time, sender=user.uid, exp='false')
+		connection = engine.connect()
+		res = connection.execute(alarm_entry)
+		connection.close()
+
+		#Return plain text response for ajax call.
+		alarm_msg = friend + " has received an alarm for " + date + " at " + hh + ":" + mm + " " + mers
+		resp = make_response(alarm_msg);
+		return resp;
+
+	return redirect("/logout")
+
+
+@app.route('/alarms', methods=('GET', 'POST'))
+def getalarms():
+	user = request.args.get('user');
+	alarms = Table('detailed_alarms', metadata, autoload=True)
+	user_alarms = alarms.select(alarms.c.uid == user and alarms.c.exp == 'false').execute()
+	return render_template('alarms.xml', user_alarms=user_alarms)
 
 @app.route('/logout')
 def logout():
@@ -119,6 +148,86 @@ def logout():
 	resp = make_response(redirect('/'))
 	resp.set_cookie('session_token', '');
 	return resp;
+
+def dashboard(email, password):
+
+ 	try:
+		user = users.select(users.c.email == email).execute().first()
+		username = user.username
+
+		#check that passwords match
+		if check_password_hash(password, user.password, user.salt):
+
+			#set session hash
+			session_token = generate_password_hash(email, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+			stmt = update(users).where(users.c.email==email).values(session_token=session_token)
+			connection = engine.connect()
+			connection.execute(stmt)
+			connection.close()
+
+			resp = make_response(redirect('/'))
+			resp.set_cookie('session_token', session_token);
+			return resp;
+		else:
+			error_msg = "Incorrect Password"
+			return render_template('/index.html', error_msg=error_msg)
+
+	except AttributeError:
+		error_msg = "no user exists"
+		return render_template('index.html', error_msg=error_msg)
+
+
+def addfriend(newFriend):
+	friends = getfriends()
+	user = validate_login()
+	new_friend = users.select(users.c.email == newFriend.email.data).execute().first()
+
+	if (new_friend and friend.select(friend.c.id1 == new_friend.uid and friend.c.id2 == user.uid).execute().first() ):
+		msg = new_friend.username + " is already in your friendlist."
+		return render_template('dashboard.html', username=user.username, alert=msg, newfriend=newFriend, friends=friends, permissions=getpermissions())
+	elif (new_friend):
+		friend_entry = friend.insert().values(id1=new_friend.uid, id2=user.uid, request=2)
+		connection = engine.connect()
+		res = connection.execute(friend_entry)
+		connection.close()
+		print ["added", new_friend.username]
+		msg = new_friend.username + " has been added to your friends."
+		return render_template('dashboard.html', username=user.username, alert=msg, newfriend=newFriend, friends=friends, permission=getpermissions())
+	else:
+		#TODO: SEND EMAIL
+		return "friend not registered"
+
+	
+def getfriends():
+	#get friendlist
+	friends = Table('detailed_friends', metadata, autoload=True)
+	friend_choices = friends.select(friends.c.permission == True).execute()
+	return friend_choices
+
+def getpermissions():
+
+	permissions=[]
+	user = validate_login()
+	friendlist = getfriends()
+
+	for f in friendlist:
+		alert_them = friend.select(friend.c.id1 == f.id1 and friend.c.id2 == user.uid and permission==True).execute().first()
+		alert_you = friend.select(friend.c.id1 == user.uid and friend.c.id2 == f.id1 and permission==True).execute().first()
+
+		if (alert_them):
+			alert_them = "Yes"
+		else: 
+			alert_them = "No"
+
+		if (alert_you):
+			alert_you = "Yes"
+		else:
+			alert_you = "No"
+
+		permissions.append([f.username, alert_them, alert_you, f.id1])
+	
+
+	return permissions
 
 if __name__ == '__main__':
 	app.run()
